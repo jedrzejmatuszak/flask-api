813956d76925bec77f70fa78bad1001fa632dddf
jedrzejmatuszak <jedrzej.matuszak@gmail.com>
2019-06-27 11:33:59 +0200

    populate database

--- app/api/hits.py
+++ app/api/hits.py
@@ -1,11 +1,20 @@
 from app.api import bp
 
 
+# na POST tworzy nowy hit na podstawie przekazanego obiektu: artistID, title
 @bp.route('/api/v1/hits')
 def get_hits():
     pass
 
 
+# można zaktualizować tylko: artistId, 'title' oraz 'title_url' + automatyczne
+# wypełnienie pola updated_at
 @bp.route('/api/v1/hits/<string:title_url>')
 def get_title(title_url):
+    pass
+
+
+# usuwa wybrany hit
+@bp.route('/api/v1/hits/<string:title_url>')
+def delete_hit(title_url):
     pass
